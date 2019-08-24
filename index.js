da646a16689d51b8118c33d667dcef9b74280efe
Belayet Hossain <soykethandymama@gmail.com>
2019-08-24 14:37:36 +0600

    multiple image upload complete

--- index.js
+++ index.js
@@ -31,10 +31,33 @@ const upload = multer({
         }
     }
 });
+
+const uploads = multer({
+    storage: store,
+    limits: {fileSize: 1000000},
+    fileFilter: function (req, file, cb) {
+        const fileExts = ['png', 'jpg', 'jpeg', 'gif', 'webp'];
+        const isAllowedExt = fileExts.includes(file.originalname.split('.')[1].toLowerCase());
+        const isAllowedMimeType = file.mimetype.startsWith("image/");
+        if (isAllowedExt && isAllowedMimeType) {
+            return cb(null, true)
+        }
+        else {
+            cb('File type not allowed!')
+        }
+    }
+});
+
 const photoUrl = [
     {id: '1', photo: 'images/photo-1566372491949'},
     {id: '2', photo: 'images/photo-1566372491949'},
     {id: '3', photo: 'images/photo-1566372491949'}
+];
+
+const photoUrls = [
+    {id: '1', photo: ['images/photo-1566372491949', 'images/photos-1566627651390']},
+    {id: '2', photo: ['images/photos-1566627651387', 'images/photos-1566627651390']},
+    {id: '3', photo: ['images/photos-1566627651390', 'images/photos-1566627651387']}
 ];
 
 const resize = async (req, res, next) => {
@@ -59,12 +82,44 @@ const resize = async (req, res, next) => {
     }
 };
 
+const resizes = async (req, res, next) => {
+    if (!req.files) {
+        next();
+        return;
+    }
+    req.files.forEach(async p => {
+        console.log(p);
+        const extension = p.mimetype.split('/')[1];
+        req.body.photos =`${uuid.v4()}.${extension}`;
+        console.log(req.body.photos);
+        const [perr, photo] = await _p(jimp.read(p.buffer));
+        console.log(p.buffer);
+    });
+
+    // console.log(req.body.photos);
+    // const extension = req.files.mimetype.split('/')[1];
+    // req.body.photos = `${uuid.v4()}.${extension}`;
+    // const [perr, photo] = await _p(jimp.read(req.file.buffer));
+    // if (!perr) {
+    //     if( photo.bitmap.width > 400 ) {
+    //         photo.resize(400, jimp.AUTO);
+    //         photo.quality(60);
+    //         photo.write(`./images/${req.body.photo}`);
+    //         next();
+    //     }
+    //     photo.write(`./images/${req.body.photo}`);
+    //     next();
+    // } else {
+    //     return reject(perr.message);
+    // }
+};
+
 //get
 app.get('/', (req, res) => {
     res.json({message: 'Hello World!'});
 });
 
-//get file by id
+//get image by id
 app.get('/:id', (req, res) => {
     const photoFound = photoUrl.find( p => p.id ===  req.params.id);
     if(photoFound) {
@@ -75,35 +130,45 @@ app.get('/:id', (req, res) => {
 });
 
 // upload single image
-app.post('/profile', upload.single('photo'), resize, async (req, res) => {
+app.post('/profile', upload.single('photo'), resize, (req, res) => {
     const photoExist = photoUrl.find( p => p.id ===  req.body.id);
+    console.log(req.file);
     if (!photoExist) {
         const data = {
             id: req.body.id,
             photo: req.body.photo
         };
-        await photoUrl.push(data);
-        res.send({'message': 'File uploaded successfully'});
+        photoUrl.push(data);
+        res.send(photoExist);
     }else{
         const id = photoExist.id;
-        const pindex = await photoUrl.findIndex(p => p.id === id);
+        const pindex = photoUrl.findIndex(p => p.id === id);
         photoUrl[pindex].photo = req.body.photo;
         res.json({message: 'photo exist', photoUrl });
     }
 
 });
 
-// // upload multiple image
-// app.post('/api/photo',function(req,res){
-//     upload(req,res,function(err) {
-//         //console.log(req.body);
-//         //console.log(req.files);
-//         if(err) {
-//             return res.end("Error uploading file.");
-//         }
-//         res.end("File is uploaded");
-//     });
-// });
+// upload multiple image
+app.post('/photos', uploads.array('photos', 2), (req, res) => {
+console.log(req.files);
+    const photoExist = photoUrls.find( p => p.id ===  req.body.id);
+    const photos = [];
+    req.files.forEach(e => photos.push(e.path));
+    if (!photoExist) {
+        const data = {
+            id: req.body.id,
+            photos: photos
+        };
+        photoUrls.push(data);
+        res.send({'message': 'File uploaded successfully', data});
+    }else{
+        const id = photoExist.id;
+        const pindex = photoUrls.findIndex(p => p.id === id);
+        photoUrls[pindex].photo = photos;
+        res.json({message: 'photo exist', photoUrls });
+    }
+});
 
 
 app.listen(port, () => console.log(`server listening on port ${port}...`));
